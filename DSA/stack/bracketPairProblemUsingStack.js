a9544ba1394ebb4b65ad2f44d517cb5eff13e3cc
Shubh-patel121 <shubham.patidar2013@gmail.com>
2024-04-17 21:58:31 +0530

    [stack]: bracket pair matching implementation

---
+++ DSA/stack/bracketPairProblemUsingStack.js
@@ -0,0 +1,35 @@
+let testingString = "{()}" ;
+
+const checkCorrectBracketPairOrNot = (str) => {
+    const stack = [] ;
+    const BRACKETS = {
+        "(" :")",
+        "[": "]",
+        "{": "}"
+    }
+    const closing = [")","]","}"];
+    for (const char of str) {
+
+        if(BRACKETS[char]){
+            stack.push(char);
+        }else if(closing.includes(char)){
+            /*  if stack length is 0 means no opening brackets, in such case if we are getting
+             closing bracket then this is not correct order */
+            if(stack.length === 0) return false ;
+
+            /* now if we are getting closing brackets means all opening brackets are already
+             in stack and we have to check whether it is correct closing bracket or not; */
+
+             let top = stack.pop(); // it will have opening bracket bec we only pushed for open brackets in stack
+             if(BRACKETS[top] !== char){
+                return false
+             }
+        }
+
+        
+    }
+
+    return stack.length === 0 ;
+}
+
+console.log("is correct bracket pair matched string:", checkCorrectBracketPairOrNot(testingString));
---
+++ DSA/stack/stack.js
@@ -0,0 +1,26 @@
+/* 
+
+A stack is a fundamental data structure in computer science that follows the Last In, First Out (LIFO) principle. It can be visualized as a collection of elements with two primary operations: push and pop.
+
+Here are the key characteristics of a stack:
+
+Last In, First Out (LIFO): The last element added to the stack is the first one to be removed. This behavior is analogous to a stack of plates, where you can only add or remove plates from the top.
+
+Operations:
+
+Push: Adding an element to the top of the stack.
+Pop: Removing the top element from the stack.
+Peek (or Top): Retrieving the top element without removing it.
+IsEmpty: Checking if the stack is empty.
+Size: Returning the number of elements in the stack.
+Implementation:
+
+Stacks can be implemented using various data structures, such as arrays or linked lists.
+In array-based implementation, push and pop operations have O(1) time complexity, but resizing the array may require O(n) time in some cases.
+In linked list-based implementation, push and pop operations also have O(1) time complexity, and resizing is not an issue.
+Applications:
+
+Stacks are widely used in programming languages for function call management (call stack).
+They are used in algorithms like depth-first search (DFS), backtracking, and expression evaluation.
+Undo mechanisms in text editors or applications often use stacks to store the history of operations.
+*/
