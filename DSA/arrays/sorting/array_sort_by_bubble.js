6d0c6d3850ad7a0cf2e905298cb340efe97f6523
Shubh-patel121 <shubham.patidar2013@gmail.com>
2026-06-24 16:13:38 +0530

    bubble sorting improvement

--- DSA/arrays/sorting/array_sort_by_bubble.js
+++ DSA/arrays/sorting/array_sort_by_bubble.js
@@ -1,16 +1,24 @@
-let sampleArray = [3,6,2,55,34,24,11,68,2,2,3,3];
+let sampleArray = [3, 6, 2, 55, 34, 24, 11, 68, 2, 2, 3, 3];
 
 const sortByBubble = (data) => {
-    for(let i=0 ; i < data.length-1 ; i++){
-        for(let j=0 ; j < data.length ; j++){
-             if(data[j] > data[j+1]){
-                let dataAtJBeforeSwap = data[j]
-                data[j] = data[j+1];
-                data[j+1] = dataAtJBeforeSwap;
-             }
-        }
+  let n = data.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false; // Optimization tracking
+
+    // Notice the boundary: 'n - 1 - i'
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (data[j] > data[j + 1]) {
+        // Modern ES6 Array Destructuring Swap (no temp variable needed!)
+        [data[j], data[j + 1]] = [data[j + 1], data[j]];
+        swapped = true;
+      }
     }
-    return data ;
-}
+
+    // If no two elements were swapped by the inner loop, array is sorted!
+    if (!swapped) break;
+  }
+  return data;
+};
 
 console.log("sorted array is:", sortByBubble(sampleArray));
